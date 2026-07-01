c18995347a4f3b7bfbeb69c3ed86c96f6917d5bf
Nayeemshaik29 <bashanayeem79@gmail.com>
2026-07-01 21:03:58 +0530

    feat: implement repository layer for Order

---
+++ order-service/src/main/java/com/quickbite/order_service/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.quickbite.order_service.repository;
+
+
+import com.quickbite.order_service.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+}
